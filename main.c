8d308b50866f31503f30e1544cd42fa61cee6646
aheir <aheir@aheir.com>
2021-06-22 22:52:09 -0300

    cambios no destacables

--- main.c
+++ main.c
@@ -1,19 +1,39 @@
+/**
+ * @file main.c
+ * @author 
+ * @brief Archivo principal del proyecto. Inicializa threads y la FSM.
+ * @version 0.1
+ * @date 2021-06-22
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include <stdlib.h>
 
 #include "global.h"
 #include "fsm.h"
 #include "queue.h"
+#include "termlib.h"
+#include "events.h" 
 
-STATE *p2state=NULL;	/*Used to store FSM state*/
+STATE* p2CurrentState = NULL;	
 
 int main (void)
 {
+	//Limpio pantalla
 	clrscr();
-    char evento;
-	p2state=FSM_GetInitState();// Inicializo la FSM con el estado inicial
 
-	while(evento = getEvento()){
-		p2state = fsm(p2state, evento);
+	//Variable de eventos
+    event_t evento;
+
+	//Inicializo la FSM con el estado inicial
+	p2CurrentState = fsm_getInitState();
+
+	//Adquiero eventos y corro la FSM
+	while((evento = getEvent()))
+	{
+		p2CurrentState = fsm(p2CurrentState, evento);
 	}
 
 	return 0;
