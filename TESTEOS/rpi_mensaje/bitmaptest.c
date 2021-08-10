cde7721d732ce69cd77b951a7190d216ebbcc15a
tcastro-cyber <tomacastro@itba.edu.ar>
2021-08-10 18:44:54 -0300

    mensaje que mueve

--- TESTEOS/rpi_mensaje/bitmaptest.c
+++ TESTEOS/rpi_mensaje/bitmaptest.c
@@ -5,9 +5,9 @@
 #include <time.h>
 
 
-Renglon renglon = {0}, reserva = {0};
-Matriz matriz = {0};
-int i;
+Renglon renglon = {0}, reserva = {0}; //RENGLON: el renglón que aparece en la matriz física, RESERVA: el renglón imaginario que extiende a RENGLON para mover el mensaje (RENGLON:RESERVA)
+Matriz matriz = {0};//donde se graba renglón 
+int i;//por cuál letra voy leyendo a msj
 char msj[] = "ABMMMAB";
 
 void *thread(){
@@ -18,7 +18,7 @@ void *thread(){
         escribirRenglon(matriz, renglon, 5);
         actualizarDisplay(matriz);
 
-        if(!renglonBool(reserva)){
+        if(!renglonBool(reserva)){ //si la reserva queda vacía, espero un tiempo (para agregar el espacio) y relleno la reserva con letras nuevas
             inicio = clock();
             while((clock() - inicio)/(CLOCKS_PER_SEC) < 0.05);
             renglonDobleShiftIzq(renglon, reserva, 1);
@@ -28,7 +28,7 @@ void *thread(){
             int j=0;
             int resto = 16;
             for(; resto >= 0; i++){
-                if(i == sizeof(msj)) i = 0;
+                if(i == sizeof(msj)) i = 0; //si termino la palabra vuelvo a empezar desde el principio
                 char c = msj[i] ? msj[i] : ' ';
                 Renglon letra;
                 CharARenglon(c, letra);
@@ -50,32 +50,32 @@ int main(){
     disp_init();       //inicializa el display
     disp_clear();      //limpia todo el display
   
-    int j = 0; //donde voy a escribir la proxima vez
-    int mover = 0; //si debo o n mover el mensaje despues
-    int resto = 16;
+    int j = 0; //a partir de donde voy a escribir la proxima vez
+    int mover = 0; //si debo o no mover el mensaje despues (se activa cuando termino de escribir en renglon y quedan letras aún)
+    int resto = 16; //cuantos espacios me quedarían si escribo la próxima letra
 
-    for(i=0; i<(sizeof(msj)) && !(mover && resto < 0); i++){
-        char c = msj[i] ? msj[i] : ' ';
+    for(i=0; i<(sizeof(msj)) && !(mover && resto < 0); i++){ //rellena el mensaje por primera vez
+        char c = msj[i] ? msj[i] : ' '; //el caracter que debo escribir (si es el terminador escribo espacio)
 
         Renglon letra;
-        CharARenglon(c, letra);
+        CharARenglon(c, letra); //letra contiene la letra provisoria pasada a renglón
 
-        int espacios = c == ' ' ? longitudes[26] : longitudes[c-'A'];
+        int espacios = c == ' ' ? longitudes[26] : longitudes[c-'A']; //calculo los espacios horizontales que ocupará la letra
 
         if(!mover && resto < 0){//paso entre el display y la reserva
             mover = 1;
-            renglonDobleShiftDer(letra, reserva, j);
+            renglonDobleShiftDer(letra, reserva, j); //muevo la letra para que quede entre medio de ambos renglones (sin perder información)
             renglonOr(renglon, letra);
-            j = j + espacios + 1 - 16;
+            j = j + espacios + 1 - 16; //dejo un espacio entre letra y letra
         }
         else if(mover){
-            renglonShiftDer(letra, j);
+            renglonShiftDer(letra, j); //muevo la letra sobre renglon
             renglonOr(reserva, letra);
             j += espacios + 1; //dejo un espacio entre letra y letra
             
         }
         else{
-            renglonShiftDer(letra, j);
+            renglonShiftDer(letra, j); //muevo la letra sobre reserva
             renglonOr(renglon, letra);
             j += espacios + 1; //dejo un espacio entre letra y letra
         }
