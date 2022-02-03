2a45560696050556387a92ffe198cf9751b70f68
tcastro-cyber <tomacastro@itba.edu.ar>
2022-02-03 12:46:31 -0300

    ulong

--- src/platform/rpi/bitmap.c
+++ src/platform/rpi/bitmap.c
@@ -1,12 +1,8 @@
 /**
- * @file bitmap.c
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2022-01-22
- * 
- * @copyright Copyright (c) 2022
- * 
+ * @file 	bitmap.c
+ * @authors	AGRIPPINO, ALVAREZ, CASTRO, HEIR
+ * @brief 	Archivo para manejo de matrices 16x16
+ * @copyright Copyright (c) 2022 ~ Ingeniería Electrónica ~ ITBA
  */
 
 /*******************************************************************************
@@ -17,92 +13,53 @@
 
 #include <stdio.h>
 
-
-/*******************************************************************************
- * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
- ******************************************************************************/
-
-
-
-/*******************************************************************************
- * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
- ******************************************************************************/
-
-
-
-/*******************************************************************************
- * VARIABLES WITH GLOBAL SCOPE
- ******************************************************************************/
-
-// +ej: unsigned int anio_actual;+
-
-
-/*******************************************************************************
- * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
- ******************************************************************************/
-
-/*******************************************************************************
- * ROM CONST VARIABLES WITH FILE LEVEL SCOPE
- ******************************************************************************/
-
-// +ej: static const int temperaturas_medias[4] = {23, 26, 24, 29};+
-
-
-/*******************************************************************************
- * STATIC VARIABLES AND CONST VARIABLES WITH FILE LEVEL SCOPE
- ******************************************************************************/
-
-// +ej: static int temperaturas_actuales[4];+
-
-
 /*******************************************************************************
  *******************************************************************************
                         GLOBAL FUNCTION DEFINITIONS
  *******************************************************************************
  ******************************************************************************/
 
-void printMatriz(matriz_t a){
-    for(int i=0; i<CANT_FILAS; i++, putchar('\n'))
-        for(int j=0; j<CANT_FILAS; j++)
-            putchar((a[i] & (0b1000000000000000 >> j)) ? '1' : '.'); 
-    
-    putchar('\n');
+void printMatriz(matriz_t a)
+{
+  for (int i = 0; i < CANT_FILAS; i++, putchar('\n'))
+    for (int j = 0; j < CANT_FILAS; j++)
+      putchar((a[i] & (0b1000000000000000 >> j)) ? '1' : '.');
+
+  putchar('\n');
 }
 
-void limpiarMatriz(matriz_t a){
-    for(int i=0; i<CANT_FILAS; i++)
-        a[i] = 0;
+void limpiarMatriz(matriz_t a)
+{
+  for (int i = 0; i < CANT_FILAS; i++)
+    a[i] = 0;
 }
 
-void copiarMatriz(matriz_t destino, matriz_t desde){
-    for(int i=0; i<CANT_FILAS; i++)
-        destino[i] = desde[i];
+void copiarMatriz(matriz_t destino, const matriz_t desde)
+{
+  for (int i = 0; i < CANT_FILAS; i++)
+    destino[i] = desde[i];
 }
 
-void matrizAnd(matriz_t a, matriz_t b){
-    for(int i=0; i<CANT_FILAS; i++)
-        a[i] &= b[i];
+void matrizAnd(matriz_t a, matriz_t b)
+{
+  for (int i = 0; i < CANT_FILAS; i++)
+    a[i] &= b[i];
 }
 
-void matrizOr(matriz_t a, matriz_t b){
-    for(int i=0; i<CANT_FILAS; i++)
-        a[i] |= b[i];
+void matrizOr(matriz_t a, matriz_t b)
+{
+  for (int i = 0; i < CANT_FILAS; i++)
+    a[i] |= b[i];
 }
 
-void matrizXor(matriz_t a, matriz_t b){
-    for(int i=0; i<CANT_FILAS; i++)
-        a[i] ^= b[i];
+void matrizXor(matriz_t a, matriz_t b)
+{
+  for (int i = 0; i < CANT_FILAS; i++)
+    a[i] ^= b[i];
 }
 
-void matrizNot(matriz_t a){
-    for(int i=0; i<CANT_FILAS; i++)
-        a[i] = ~a[i];
+void matrizNot(matriz_t a)
+{
+  for (int i = 0; i < CANT_FILAS; i++)
+    a[i] = ~a[i];
 }
-
-
-/*******************************************************************************
- *******************************************************************************
-                        LOCAL FUNCTION DEFINITIONS
- *******************************************************************************
- ******************************************************************************/
-
